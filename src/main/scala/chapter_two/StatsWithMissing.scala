7a0e7660be3c46ad6bc2394babd93910dd7cd655
mikejlong60 <mjlong@functionicity.com>
2016-05-10 15:21:31 -0400

    Completed chapter 2.

--- src/main/scala/chapter_two/StatsWithMissing.scala
+++ src/main/scala/chapter_two/StatsWithMissing.scala
@@ -45,3 +45,12 @@ def statsWithMissing(rdd: RDD[Array[Double]]): Array[NAStatCounter] = {
     n1.zip(n2).map { case (a, b) => a.merge(b)}
   })
 }
+
+def naz(d: Double) = if (Double.NaN.equals(d)) 0.0 else d
+
+case class Scored(md: MatchData, score: Double)
+
+val ct = parsed.map(md => {
+  val score = Array(2,5,6,7,8).map(i => naz(md.scores(i))).sum
+  Scored(md, score)
+})
